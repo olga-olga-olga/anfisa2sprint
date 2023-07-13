cbf1cc7dfc3d9f32f4036066a5547d5f7350c198
Ольга Чернецкая <chernetzkayaolga@yandex.by>
2023-07-13 15:27:54 +0300

    настроена админзона

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('homepage.urls')),
     path('about/', include('about.urls')),
     path('ice_cream/', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
 ]
